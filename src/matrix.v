abc377feeaaa6ddc51ae7f00b601f3155f67199f
Uri Shaked <uri@urishaked.com>
2021-12-04 09:39:32 +0200

    refactor: make matrix more synthesizable
    Yosys used a huge amount of RAM for synthesizing the design, and was also very slow.
    
    This changes speeds up the synthesis, and makes it possible to synthesize larger matrices.

--- src/matrix.v
+++ src/matrix.v
@@ -20,21 +20,24 @@ module silife_matrix #(
   wire [(WIDTH*HEIGHT)-1:0] cell_values;
   generate
     for (y = 0; y < HEIGHT; y = y + 1) begin : gen_cellsy
+      localparam current_row = y * WIDTH;
+      localparam prev_row = current_row - WIDTH;
+      localparam next_row = current_row + WIDTH;
       for (x = 0; x < WIDTH; x = x + 1) begin : gen_cellsx
         silife_cell cell_instance (
-            .reset (reset || clear_cells[y*WIDTH+x]),
+            .reset (reset || clear_cells[current_row+x]),
             .clk   (clk),
             .enable(enable),
-            .revive(set_cells[y*WIDTH+x]),
-            .nw    (y > 0 && x > 0 ? cell_values[WIDTH*(y-1)+(x-1)] : 1'b0),
-            .n     (y > 0 ? cell_values[WIDTH*(y-1)+x] : 1'b0),
-            .ne    (y > 0 && x < WIDTH - 1 ? cell_values[WIDTH*(y-1)+x+1] : 1'b0),
-            .e     (x < WIDTH - 1 ? cell_values[WIDTH*(y)+x+1] : 1'b0),
-            .se    (y < HEIGHT - 1 && x < WIDTH - 1 ? cell_values[WIDTH*(y+1)+x+1] : 1'b0),
-            .s     (y < HEIGHT - 1 ? cell_values[WIDTH*(y+1)+x] : 1'b0),
-            .sw    (y < HEIGHT - 1 && x > 0 ? cell_values[WIDTH*(y+1)+x-1] : 1'b0),
-            .w     (x > 0 ? cell_values[WIDTH*(y)+x-1] : 1'b0),
-            .out   (cell_values[WIDTH*(y)+x])
+            .revive(set_cells[current_row+x]),
+            .nw    (y > 0 && x > 0 ? cell_values[prev_row+(x-1)] : 1'b0),
+            .n     (y > 0 ? cell_values[prev_row+x] : 1'b0),
+            .ne    (y > 0 && x < WIDTH - 1 ? cell_values[prev_row+x+1] : 1'b0),
+            .e     (x < WIDTH - 1 ? cell_values[current_row+x+1] : 1'b0),
+            .se    (y < HEIGHT - 1 && x < WIDTH - 1 ? cell_values[next_row+x+1] : 1'b0),
+            .s     (y < HEIGHT - 1 ? cell_values[next_row+x] : 1'b0),
+            .sw    (y < HEIGHT - 1 && x > 0 ? cell_values[next_row+x-1] : 1'b0),
+            .w     (x > 0 ? cell_values[current_row+x-1] : 1'b0),
+            .out   (cell_values[current_row+x])
         );
       end
     end
