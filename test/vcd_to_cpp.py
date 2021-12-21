2203de3b055cb77dd17dc6c56a3fd40b324be2ef
Uri Shaked <uri@urishaked.com>
2021-12-21 22:48:28 +0200

    feat(vcd_to_cpp): accept input file name as argument
    also add arguments to control the output

--- test/vcd_to_cpp.py
+++ test/vcd_to_cpp.py
@@ -1,3 +1,11 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("vcdfile")
+parser.add_argument("--comments", action='store_true')
+parser.add_argument("--timestamps", action='store_true')
+args = parser.parse_args()
+
 filter_vars = ["spi_cs", "spi_sck", "spi_mosi", "max7219_busy"]
 filter_var_ids = {}
 
@@ -10,6 +18,21 @@ def pin_names(pin_values):
     return ",".join(result)
 
 
+def print_entry(value, comment):
+    print("  {},{}".format(value, " " + comment if args.comments else ""))
+
+
+def print_delay():
+    global current_time, last_time
+    if args.timestamps:
+        print_entry(current_time - last_time, "/* ts: {} */".format(current_time))
+
+
+def print_pins():
+    global pin_values
+    print_entry(pin_values, "/* pins: {} */".format(pin_names(pin_values)))
+
+
 for index, var in enumerate(filter_vars):
     print("#define PIN_{} (0x{:02x})".format(var.upper(), 1 << index))
 print("")
@@ -20,7 +43,7 @@ definition_section = True
 last_time = 0
 current_time = 0
 pin_values = 0
-for line in open("max7219_tb.vcd"):
+for line in open(args.vcdfile):
     line = line.strip()
     parts = line.split(" ")
     if line.startswith("$var"):
@@ -35,9 +58,9 @@ for line in open("max7219_tb.vcd"):
         if not (var_id in filter_var_ids):
             continue
         if current_time != last_time:
-            print("  {}, /* ts: {} */".format(current_time - last_time, current_time))
+            print_delay()
+            print_pins()
             last_time = current_time
-            print("  {}, /* pins: {} */".format(pin_values, pin_names(pin_values)))
         var_id = line[1]
         pin_index = filter_vars.index(filter_var_ids[var_id])
         if line[0] == "1":
@@ -46,8 +69,7 @@ for line in open("max7219_tb.vcd"):
             pin_values &= ~(1 << pin_index)
 
 if current_time != last_time:
-    print("  {}, /* ts={} */".format(current_time - last_time, current_time))
-    last_time = current_time
-    print("  {}, /* pins={} */".format(pin_values, pin_names(pin_values)))
+    print_delay()
+    print_pins()
 
 print("};")
